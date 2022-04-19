ecd37b265f7d5887271fe0a2a42e369de36a9949
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2022-04-20 00:43:37 +0100

    starting the concepts

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import MeuPrimeiroComponent from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
     BrowserModule,
---
+++ first-project/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,16 @@
+
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector:"meu-primeiro-component",
+  template: `
+  <p>Meu primeiro component em angula</p>
+  `
+})
+export default class MeuPrimeiroComponent{
+
+
+
+
+}
